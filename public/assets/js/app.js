44cbd5deb0866f41ac6f8be7ec3f9d5d91cbf85e
Morshed Alam <ma.morshedalampro@gmail.com>
2022-06-12 11:17:00 +0600

    home view complete

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,3 +6,34 @@ function toggleView(section) {
 
      toggleSection.className = 'show';
 }
+
+// FUNCTIONS
+const createHome = (bookList) => {
+     const view = document.getElementById('home-section');
+     const container = view.querySelector('.container');
+
+
+     bookList.forEach(book => {
+          const newDiv = document.createElement('div');
+          newDiv.innerHTML = `<img src="assets/img/robindonath-ekhane-kkhono-khete-asenni.jpg" alt="">
+                              <div class="text">
+                                   <h2>${book.name}</h2>
+                                   <p>Author: <span>${book.author}</span></p>
+                                   <p>Category: <span>${book.category}</span></p>
+                              </div>`
+          newDiv.className = "item";
+
+          container.appendChild(newDiv);
+     })
+}
+
+// DATA VIEW
+window.onload = function () {
+     axios.get('/api')
+          .then(({ data }) => {
+               if (data.length > 0) {
+                    createHome(data)
+               }
+          })
+          .catch(err => { alert(err) })
+}
