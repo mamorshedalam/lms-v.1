2d089554da6ab284ae0e0c7a215745a2fcbc1ce7
Morshed Alam <ma.morshedalampro@gmail.com>
2022-06-15 07:45:12 +0600

    public view made

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,6 @@
+let DATABASE = [];
+let CATEGORY = [];
+let AUTHOR = [];
 // SECTION TOGGLE
 function toggleView(section) {
      const toggleSection = document.getElementById(section);
@@ -7,6 +10,36 @@ function toggleView(section) {
      toggleSection.className = 'show';
 }
 
+function createList(item, lists) {
+     const ul = item.querySelector('ul');
+
+     for (let i = 0; i < lists.length; i++) {
+          const list = document.createElement('li');
+          list.className = 'list-item';
+          list.innerText = lists[i].name;
+
+          ul.appendChild(list);
+     }
+}
+
+function createSection(section, criteria) {
+     const targetSection = document.getElementById(section);
+     const container = targetSection.querySelector('.container');
+
+     criteria.forEach(item => {
+          let books = DATABASE.filter(book => book.author == item || book.category == item);
+
+          const field = document.createElement('div');
+          field.className = 'item';
+          field.innerHTML = `<h2>${item} <span>${books.length}</span></h2>
+                              <ul></ul>`;
+
+          createList(field, books);
+
+          container.appendChild(field);
+     })
+
+}
 // FUNCTIONS
 const createHome = (bookList) => {
      const view = document.getElementById('home-section');
@@ -27,12 +60,17 @@ const createHome = (bookList) => {
      })
 }
 
-const createCategory = (bookList) => {
-     bookList.forEach(book => {
-          if (book.category == "Mystery") {
+const createCategory = (database) => {
+     database.forEach(book => {
+          const author = book.author;
+          const category = book.category;
 
+          if (!AUTHOR.includes(author)) {
+               AUTHOR.push(author);
           }
-
+          if (!CATEGORY.includes(category)) {
+               CATEGORY.push(category);
+          }
      })
 }
 
@@ -41,8 +79,9 @@ window.onload = function () {
      axios.get('/api')
           .then(({ data }) => {
                if (data.length > 0) {
-                    createHome(data);
-                    createCategory(data);
+                    DATABASE = data;
+                    createHome(DATABASE);
+                    createCategory(DATABASE);
                }
           })
           .catch(err => { alert(err) })
